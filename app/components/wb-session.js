df994a229f66139e46e62786d8c1f4d723128b05
Martin Hecher <martin.hecher@fraunhofer.at>
2015-03-21 01:21:24 +0100

    * changed style * added header toggle to 'wb-session'

--- app/components/wb-session.js
+++ app/components/wb-session.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 import layout from '../templates/components/wb-session';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+
+  showHeader: false
 });
