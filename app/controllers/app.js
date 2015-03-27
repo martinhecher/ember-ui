05df8ee107e498e81efa4b1dc4f1afa61e185a04
Martin Hecher <martin.hecher@fraunhofer.at>
2015-03-27 15:27:17 +0100

    added file deselection support

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -3,8 +3,14 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
 	actions: {
 		onFileSelection: function(file) {
-			var selected = this.get('app.selectedFiles');
-			this.get('app.selectedFiles').pushObject(file);
+			var selected = this.get('app.selectedFiles'),
+				presentFile = selected.findBy('path', file.get('path'));
+
+			if (presentFile) {
+				selected.removeObject(presentFile);
+			} else {
+				this.get('app.selectedFiles').pushObject(file);
+			}
 		}
 	}
 });
